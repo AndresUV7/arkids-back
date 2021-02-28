dfab6ce053ff577d40d439bab866786653965644
heroe-geek <heroe-geek@users.noreply.github.com>
2021-02-28 10:25:03 -0500

    Initial commit 2

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb+srv://arkids21:IpC@_2120@ar-kids.hxmrr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const URI = 'mongodb+srv://arkids21:IpC@_2120@ar-kids.hxmrr.mongodb.net/crud?retryWrites=true&w=majority';
 
 mongoose.connect(URI , { useUnifiedTopology: true, useNewUrlParser: true })
     .then(db => console.log('Conectamos a base'))
