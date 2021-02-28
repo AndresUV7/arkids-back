bdfe1d100c2b4958185b47707b3aeb333022a3fe
heroe-geek <heroe-geek@users.noreply.github.com>
2021-02-28 12:46:10 -0500

    Initial commit 7

--- backend/controllers/persona.controller.js
+++ backend/controllers/persona.controller.js
@@ -138,39 +138,39 @@ personaCtrl.actualizarContrasena = async (req, res) => {
         }); 
     }
 
-    var resettoken = new passwordResetToken({ _userId: cedula, resettoken: crypto.randomBytes(16).toString('hex') });
+    // var resettoken = new passwordResetToken({ _userId: cedula, resettoken: crypto.randomBytes(16).toString('hex') });
 
     
-    await resettoken.save().then(doc => {
+    // await resettoken.save().then(doc => {
         
-            console.log(doc)
-    });
+    //         console.log(doc)
+    // });
   
     
-    let transporter = nodemailer.createTransport({
-        service: 'gmail', // true for 465, false for other ports
-        auth: {
-          user: 'arkids2021.info@gmail.com', // generated ethereal user
-          pass: 'IpC@_2120', // generated ethereal password
-        },
-      });
+    // let transporter = nodemailer.createTransport({
+    //     service: 'gmail', // true for 465, false for other ports
+    //     auth: {
+    //       user: 'arkids2021.info@gmail.com', // generated ethereal user
+    //       pass: 'IpC@_2120', // generated ethereal password
+    //     },
+    //   });
 
 
     
-      // send mail with defined transport object
-      let info = await transporter.sendMail({
-        from: 'arkids2021.info@gmail.com', // sender address
-        to: "heroes.geek2021@gmail.com", // list of receivers
-        subject: "Hello ✔", // Subject line
-        text: "Hello world?", // plain text body
-        html: "<b>Hello world?</b>"+resettoken.resettoken, // html body
-      });
+    //   // send mail with defined transport object
+    //   let info = await transporter.sendMail({
+    //     from: 'arkids2021.info@gmail.com', // sender address
+    //     to: "heroes.geek2021@gmail.com", // list of receivers
+    //     subject: "Hello ✔", // Subject line
+    //     text: "Hello world?", // plain text body
+    //     html: "<b>Hello world?</b>"+resettoken.resettoken, // html body
+    //   });
     
-      console.log("Message sent: %s", info.messageId);
-      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    //   console.log("Message sent: %s", info.messageId);
+    //   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
     
-      // Preview only available when sending through an Ethereal account
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    //   // Preview only available when sending through an Ethereal account
+    //   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 
    
     
